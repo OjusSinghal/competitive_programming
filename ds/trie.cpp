0dbcef8a8dd08f3edb8de0fab2800829802a18bb
Ojus Singhal <ojus.singhal17@gmail.com>
2021-09-13 14:45:21 +0530

    trie data structure added

--- ds/trie.cpp
+++ ds/trie.cpp
@@ -1,8 +1,10 @@
+// 32 BIT INTEGERS AS BINARY STRINGS
 // BUILD TRIE FROM INDIVIDUAL CALLS,
 // UPDATES:
-//      -> RANGE(ADD)
+//      -> ADD AN INTEGER TO THE TREE
+//      -> REMOVE AN INTEGER FROM THE TREE
 // QUERIES:
-//      -> RANGE (MIN)
+//      -> MAXIMUM XOR FOR A GIVEN INTEGER
 
 #include <algorithm>
 #include <bitset>
@@ -35,6 +37,105 @@ typedef pair<int, int> pii;
 #define S second
 #define sq(a) ((a) * (a))
 
+struct node
+{
+    node *left;
+    node *right;
+    int val; //size of subtree, including this node
+
+    node(int val)
+    {
+        this->left = nullptr;
+        this->right = nullptr;
+        this->val = val;
+    }
+};
+
+void insert(node *u, string &b)
+{
+    node *cur = u;
+    REP(i, 0, 32)
+    {
+        cur->val++;
+        if (b[i] == '0')
+        {
+            if (cur->left == nullptr)
+                cur->left = new node(0);
+            cur = cur->left;
+        }
+        else
+        {
+            if (cur->right == nullptr)
+                cur->right = new node(0);
+            cur = cur->right;
+        }
+    }
+    cur->val++;
+}
+
+void remove(node *u, string &b)
+{
+    node *cur = u;
+    REP(i, 0, 32)
+    {
+        cur->val--;
+        if (b[i] == '0')
+        {
+            if (cur->left->val == 1)
+            {
+                cur->left = nullptr;
+                return;
+            }
+            cur = cur->left;
+        }
+        else
+        {
+            if (cur->right->val == 1)
+            {
+                cur->right = nullptr;
+                return;
+            }
+            cur = cur->right;
+        }
+    }
+    cur->val--;
+}
+
+int maxor(node *u, string &b)
+{
+    string ans = "";
+    node *cur = u;
+    REP(i, 0, 32)
+    {
+        if (b[i] == '1')
+        {
+            if (cur->left)
+            {
+                cur = cur->left;
+                ans += "1";
+            }
+            else
+            {
+                cur = cur->right;
+                ans += "0";
+            }
+        }
+        else
+        {
+            if (cur->right)
+            {
+                cur = cur->right;
+                ans += "1";
+            }
+            else
+            {
+                cur = cur->left;
+                ans += "0";
+            }
+        }
+    }
+    return stoi(ans, 0, 2);
+}
 
 void run()
 {
